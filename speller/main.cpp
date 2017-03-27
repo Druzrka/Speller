9e2150f67814fe843122037fa96c4ed577b4bad9
Konstantyn <konstantyn@macbook-konstantyn.local>
2017-03-27 16:03:47 +0300

    Initial Commit

---
+++ speller/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  speller
+//
+//  Created by Захар on 27.03.17.
+//  Copyright © 2017 Захар. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
